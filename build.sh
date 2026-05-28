754683b21320de94df613915a6b09cb38134f8a1
kygm02 <kygm02@gmail.com>
2026-05-28 21:36:45 +0100

    Assignment 5 Part 2

--- build.sh
+++ build.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 #Script to build buildroot configuration
 #Author: Siddhant Jajoo
+
 source shared.sh
 export HOSTCC=$(command -v gcc-13 || command -v gcc)
 export HOSTCXX=$(command -v g++-13 || command -v g++)
 export BR2_DL_DIR=${HOME}/.dl
 export BR2_TAR_OPTIONS="--no-same-owner --no-same-permissions"
+
 EXTERNAL_REL_BUILDROOT=../base_external
 OUTPUT_DIR=/tmp/buildroot-output
 
 git submodule init
 git submodule sync
 git submodule update
-set -e
+
+set -e 
 cd `dirname $0`
 
-mkdir -p ${OUTPUT_DIR}
-
-if [ ! -e ${OUTPUT_DIR}/.config ]
+if [ ! -e buildroot/.config ]
 then
-    echo "MISSING BUILDROOT CONFIGURATION FILE"
-    if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
-    then
-        echo "USING ${AESD_MODIFIED_DEFCONFIG}"
-        make -C buildroot O=${OUTPUT_DIR} defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
-    else
-        echo "USING ${AESD_DEFAULT_DEFCONFIG}"
-        make -C buildroot O=${OUTPUT_DIR} defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG}
-    fi
-else
-    echo "USING EXISTING BUILDROOT CONFIG"
-    echo "To force update, delete .config or make changes using make menuconfig and build again."
-fi
+	echo "MISSING BUILDROOT CONFIGURATION FILE"
 
-make -C buildroot O=${OUTPUT_DIR} BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} -j$(nproc)
-rm -rf buildroot/output
-ln -sfn ${OUTPUT_DIR} buildroot/output
+	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
+	then
+		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
+		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT} -j$(nproc)
+	else
+		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
+		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
+		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG} -j$(nproc)
+	fi
+else
+	echo "USING EXISTING BUILDROOT CONFIG"
+	echo "To force update, delete .config or make changes using make menuconfig and build again."
+	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} -j$(nproc)
+
+fi
